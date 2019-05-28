6e33bbf594c8fa1031d35f662a4a9159f5a82835
EC2 Default User <ec2-user@ip-172-31-43-90.ec2.internal>
2019-05-28 15:28:03 +0000

    Add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -15,45 +16,41 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
 
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
+      flash[:success] = 'task が正常に投稿されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task が投稿されませんでした'
+      flash.now[:danger] = 'task が投稿されませんでした'
       render :new
     end
   end
-  
+
   def edit
-      @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
-
     if @task.update(task_params)
-      flash[:success] = 'Taskは正常に更新されました'
+      flash[:success] = 'task は正常に更新されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task は更新されませんでした'
+      flash.now[:danger] = 'task は更新されませんでした'
       render :edit
     end
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
-    flash[:success] = 'Task は正常に削除されました'
+    flash[:success] = 'task は正常に削除されました'
     redirect_to tasks_url
   end
-  
+
   private
 
-  # Strong Parameter
-  def task_params
-    params.require(:task).permit(:content)
+  def set_task
+    @task = Task.find(params[:id])
   end
-  
-  
-  
+
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
 end
